02c0bcfceeb77ee121def1e3bace16e986e0ad49
HanYooTae <han931120@gmail.com>
2024-08-26 23:19:31 +0900

    Mutex와 Lockguard

--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -3,38 +3,74 @@
 #include "CorePch.h"
 #include <thread>
 #include <atomic>
+#include <mutex>
 
-atomic<int32> sum = 0;
+// STL에서 제공하는 컨테이너들은 기본적으로 멀티쓰레드 환경에서 동작하지 않는다고 가정해야 함
 
-void Add()
+
+vector<int32> v;
+
+// Mutual Exclusive (상호배타적)
+mutex m;
+
+// RAII : (Resource Acquisition Is Initialization)
+template<typename T>
+class LockGuard
 {
-    for (int32 i = 0; i < 100'0000; i++)
+public:
+    LockGuard(T& m)
     {
-        //sum.fetch_add(1);
-        sum++;
+        _mutex = &m;
+        _mutex->lock();
+    }
+
+    ~LockGuard()
+    {
+        _mutex->unlock();
+    }
+
+private:
+    T* _mutex;
+};
+
+
+void Push()
+{
+    for (int32 i = 0; i < 10000; i++)
+    {
+        // 실질적으로는 싱글 쓰레드로 동작하는 것이라고 볼 수 있음
+
+        // 자물쇠 잠그기
+        //m.lock();
+        //std::lock_guard<std::mutex> lockguard(m);
+
+        // 당장 lock을 걸지 않고, 인터페이스만 만들어둠
+        std::unique_lock<std::mutex> uniqueLock(m, std::defer_lock);
+
+        // uniqueLock을 잠구는 순간에 lockguard와 동일하게 동작 (잠기는 시점을 뒤로 미룰 수 있음)
+        uniqueLock.lock();
+
+        v.push_back(i);
+
+        if (i == 5000)
+        {
+            //m.unlock();
+            break;
+        }
+
+        // 자물쇠 풀기
+        //m.unlock();
     }
 }
-
-void Sub()
-{
-    for (int32 i = 0; i < 100'0000; i++)
-    {
-        //sum.fetch_sub(1);
-        sum--;
-    }
-}
-
-// atomic사용 : Add가 먼저 실행된다면 Add가 완료될 때까지 Sub는 잠시 대기
-// 누가 먼저, 누가 대기하게 만들어주는 주체는 CPU
-// 하나가 실행될 때 다른 애가 대기 중이므로 병목현상이 발생할 수도 있음.
 
 int main()
 {
-    cout << sum << '\n';
+    //v.reserve(20000);
+    std::thread t1(Push);
+    std::thread t2(Push);
 
-    std::thread t1(Add);
-    std::thread t2(Sub);
     t1.join();
     t2.join();
-    cout << sum << '\n';
+
+    cout << v.size() << '\n';
 }
