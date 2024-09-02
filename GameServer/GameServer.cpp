afcca282f1c0111d37a95d3859836f8695ca5949
HanYooTae <han931120@gmail.com>
2024-09-02 19:20:08 +0900

    Condition_Variable

--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -10,55 +10,72 @@ mutex m;
 queue<int32> q;
 HANDLE handle;
 
+// mutex와 짝지어 동작
+// User-Level Object (커널 오브젝트X)
+// 이벤트처럼 호출
+condition_variable cv;
+
+// mutex 뿐만 아니라 일반적인 상황으로까지 확장하고 싶다면 사용
+//#include <condition_variable>
+//condition_variable_any cv;
+
+/*
+    Kelnel Object
+    - 다른 프로그램과도 이벤트를 이용하여 동기화 가능
+
+    User-Level Object
+    - 동일한 프로그램 내부에서만 사용 가능
+*/
+
+
 void Producer()
 {
     while (true)
     {
-        // 데이터를 밀어넣고 있음
-        unique_lock<mutex> lock(m);
-        q.push(100);
-    }
+        // 1) Lock을 잡고
+        // 2) 공유 변수 값을 수정
+        // 3) Lock을 풀고
+        // 4) 조건변수를 통해 다른 쓰레드에게 통보
+        {
+            unique_lock<mutex> lock(m);
+            q.push(100);
+        }
 
-    // 같은 식별자에 해당하는 커널 오브젝트를 Signal 상태로 변경(켜주는 것)
-    ::SetEvent(handle);
-    this_thread::sleep_for(100000ms);
+        cv.notify_one();    // wait 중인 쓰레드가 있으면 1개만 깨운다.
+        //::SetEvent(handle);
+        this_thread::sleep_for(100ms);
+    }
 }
 
 void Consumer()
 {
-    // 데이터를 꺼내 쓰고 있음
     while (true)
     {
-        // 커널 오브젝트가 무한 대기하도록
-        // Signal 상태라면 계속 진행
-        // Non - Signal 상태라면 대기
-        // 제 3자를 통해 무의미하게 대기하는 것을 방지해준 모습
-        ::WaitForSingleObject(handle, INFINITE);
+        //::WaitForSingleObject(handle, INFINITE);
 
         unique_lock<mutex> lock(m);
-        if (!q.empty())
+        cv.wait(lock, []() {return !q.empty(); });
+        // 1) Lock을 잡고(잡혀있다면 스킵)
+        // 2) 조건 확인
+        // - 조건이 만족하면 빠져나와서 이어서 코드를 진행
+        // - 조건이 만족하지 않으면 Lock을 풀어주고 대기 상태로 전환
+
+        // Producer함수에서 notify_one을 했으면 항상 조건식을 만족하는거 아닌가?
+        // Producer함수에서 notify_one은 lock과 별개이기 때문에 lock을 잡고 있는 것이 아니다.(Spurious Wakeup)
+        // 만약 다른 쓰레드에서 q를 pop했다면? q가 비어있을 수도 있기 때문에, 그럴 경우는 lock을 걸어줘야 함.
+
+        //while (!q.empty())
         {
             int32 data = q.front();
             q.pop();
-            cout << data << '\n';
+            cout << q.size() << '\n';
         }
     }
 }
 
 int main()
 {
-    // Kernel Object
-    // 커널 오브젝트를 관리하기 위해서 할당되는 메모리
-
-    // 커널 오브젝트의 속성
-    // 1. Usage Count : 오브젝트를 몇명이 사용하고 있는지
-    // 2. Signal(파란불) / Non - Signal(빨간불) : boolean을 사용하여 켜져있으면 Signal, 꺼져있으면 Non - Signal
-    // 3. Auto / Manual : 자동인지 수동인지
-    // CreateEvent는 커널 오브젝트들 중에서 상대적으로 가벼운 편이다.
     handle = ::CreateEvent(NULL/*보안속성*/, FALSE/*bManualReset*/, FALSE/*bInitialState*/, NULL);
-
-    // handle은 어떤 이벤트인지 구별하는 식별자가 저장이 됨.
-    // 커널 딴에서 어떤 이벤트를 사용하고 싶을 때, 몇번 식별자의 이벤트는 무엇을 해주세요 라고 명령을 할 때 handle을 사용함
 
     thread t1(Producer);
     thread t2(Consumer);
@@ -66,6 +83,5 @@ int main()
     t1.join();
     t2.join();
 
-    // 핸들을 닫아주는 함수
     ::CloseHandle(handle);
 }
