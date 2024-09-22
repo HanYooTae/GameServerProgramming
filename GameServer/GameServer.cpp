542f2c3e090707a3e9a0bfac2b76e703d51e83b5
HanYooTae <han931120@gmail.com>
2024-09-23 01:16:26 +0900

    CPU Pipeline

--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -5,77 +5,88 @@
 #include <atomic>
 #include <windows.h>
 #include <mutex>
+#include <future>
 
+// 캐시가 개입
 /*
-    CPU : 연산처리
-    RAM : 메모리()
-    캐시(cache) : 임시 저장소
-
-    데이터를 연산하기 위해서 RAM에 있는 데이터를 꺼내서 CPU에서 연산처리해줌
-
-    연산을 할 때마다 왔다갔다하는 것은 비효율적
-    -> RAM에서 데이터를 CPU에 전송하는 비용이 CPU에서 연산하는 비용보다 훨씬 더 큼
-
-    CPU의 코어는 ALU(연산 장치)와 캐시 장치로 이루어져 있음.
-    -> 캐시 장치는 위에서부터 레지스터, L1캐시, L2캐시 순서로 이루어져있으며(L3캐시 이상으로도 존재할 수 있음), 여기까지 전부 찾아봐도 데이터가 없으면 RAM에서 데이터를 가져옴.
-
-    RAM에서 데이터를 가져올 때 동일한 데이터가 또 필요할 수도 있으니 캐시에 저장하게 됨.
-
-    CPU는 연산처리를 하기 위해서 캐시 장치에 저장된 메모리 중에서 해당 주소값을 가지고 있는지 체크를 해본 후에, 이미 가지고 있는 값이라면 캐시에 있는 데이터를 연산처리함. 아니라면 RAM에서 데이터를 가져옴.
-
-    프로그램이 계속 실행되고 있을 때, 캐시 메모리에 용량이 꽉차서 더이상 저장할 수 없다면, 제일 오래되었으며 사용빈도가 가장 떨어지는 데이터를 삭제 후 새로운 데이터로 덮어씀.
-
-    캐시 설계 철학(데이터 지역성)
-     ㄴ 캐시 적중률(Cache Hit)을 높이기 위한 기법
-
-    1. Temporal Locality(시간 지역성)
-     ㄴ 방금 사용했던 데이터를 저장
-
-    2. Spatial Locality(공간 지역성)
-     ㄴ 방금 사용했던 데이터와 그 근처에 있는 데이터를 저장
+    Thread_1과 Thread_2함수 안에서 r1과 r2가 메모리에 있는 값을 불러왔다는 보장이 없다.
 */
 
-int32 buffer[10000][10000];
+/*
+    (상황1) 한쪽에서 y의 값을 수정 (y = 1)
+    (상황2) 다른 한쪽에서 y의 값을 꺼내서 읽음 (r2 = y)
+
+    1. 캐시가 개입
+    2. y의 값을 RAM에 가서 수정했다는 보장이 없음
+    3. r2가 y의 메모리를 불러왔다는 보장도 없음
+*/
+
+/*
+    ** 가시성 문제 **
+    
+    1. 멀티 쓰레드 프로그래밍이 아닌 경우라면 전혀 상관이 없다.
+    2. 단일 쓰레드라면 값을 수정했을 때, 사용하던 캐시에 수정된 값이 저장이 되어있기 때문에 상관이 없다.
+    3. 다수의 쓰레드가 개입을 하는 순간, 문제가 발생할 가능성이 생긴다.
+     ㄴ y에 1을 대입했는데, r2는 수정된 y가 아닌 수정되기 전의 y를 읽을 수도 있음.(가시성)
+     ㄴ 만약 C++이 아니라 C#이었다면, 공용 변수들 모두 volatile 키워드를 붙이면 컴파일러 최적화도 막고 가시성 문제도 해결이 되긴 하지만, 코드 재배치의 문제는 해결이 되지 않는다.
+
+     ** 코드 재배치 문제 **
+     
+     1. 코드를 작성했는데, 컴파일러가 같은 결과물이면서 코드의 순서를 뒤바꾸는게 더 효율적이라고 판단한다면 코드의 순서를 임의로 변경할 수도 있다.
+     2. 컴파일러가 순서를 뒤집지 않더라도, CPU가 순서를 뒤집을 수도 있다.
+     2-1. CPU의 파이프라인 : Fetch(명령어 가져오기), Decode(명령어 해석), Execute(명령어 실행), Write-back(결과물을 가져다주기)
+     2-2. 명령어를 해석하고 실행하는 과정에서 순서를 뒤바꾸는 것이 더 효율적이라고 컴파일러 또는 CPU가 인식은 하게 된다면 순서를 뒤바꾸는 것이다.
+*/
+
+// 가시성, 코드 재배치
+int32 x = 0;
+int32 y = 0;
+int32 r1 = 0;
+int32 r2 = 0;
+
+volatile bool ready;
+
+void Thread_1()
+{
+    while (!ready);
+
+    y = 1;  // Store y
+    r1 = x; // Load x
+}
+
+void Thread_2()
+{
+    while (!ready);
+
+    x = 1;  // Store x
+    r2 = y; // Load y
+}
 
 int main()
 {
-    memset(buffer, 0, sizeof(buffer));
+    int32 count = 0;
 
+    while (true)
     {
-        uint64 start = GetTickCount64();
+        ready = false;
+        count++;
 
-        int64 sum = 0;
+        x = y = r1 = r2 = 0;
 
-        for (int32 i = 0; i < 10000; i++)
-        {
-            for (int32 j = 0; j < 10000; j++)
-            {
-                sum += buffer[i][j];
-            }
-        }
+        thread t1(Thread_1);
+        thread t2(Thread_2);
 
-        uint64 end = GetTickCount64();
+        ready = true;
 
-        cout << "Elapsed Tick " << (end - start) << '\n';
+        t1.join();
+        t2.join();
+
+        if (r1 == 0 && r2 == 0) break;
+
+        //cout << count << '\n';
     }
-
-    {
-        uint64 start = GetTickCount64();
-
-        int64 sum = 0;
-
-        for (int32 i = 0; i < 10000; i++)
-        {
-            for (int32 j = 0; j < 10000; j++)
-            {
-                sum += buffer[j][i];
-            }
-        }
-
-        uint64 end = GetTickCount64();
-
-        cout << "Elapsed Tick " << (end - start) << '\n';
-    }
+    
+    cout << count << "번 만에 빠져나옴" << '\n';
 
     return 0;
 }
